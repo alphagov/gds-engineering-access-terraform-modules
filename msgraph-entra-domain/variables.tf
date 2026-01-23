9a342f75d51a051474273ebabaef75f2aa49f5e5
Diogo Lemos <diogo.lemos@digital.cabinet-office.gov.uk>
2026-01-23 11:58:39 +0000

    entra-domain module: add ability to make domain default

--- msgraph-entra-domain/variables.tf
+++ msgraph-entra-domain/variables.tf
@@ -8,3 +8,14 @@ variable "verify" {
   type        = bool
   default     = false
 }
+
+variable "default" {
+  description = <<-EOD
+    Whether to set this domain as default for new users. This is the same as making it the Primary domain.
+    If the value is set to 'true' the domain becomes the default.
+    It is not possible to demote the domain with a value of 'false'.
+    Demotion happens by the virtue of promoting another domain to be the default.
+  EOD
+  type        = bool
+  default     = false
+}
