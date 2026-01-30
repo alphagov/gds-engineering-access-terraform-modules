7a67da5d9053bc6b537d005812a10f53897b2a76
mikejwhat <18402492+mikejwhat@users.noreply.github.com>
2026-01-30 15:57:38 +0000

    Update example to show granular setting of policy_state

--- azurerm-conditional-access/examples/basic/locals.tf
+++ azurerm-conditional-access/examples/basic/locals.tf
@@ -11,6 +11,7 @@ locals {
       built_in_controls         = ["mfa"]
       sign_in_frequency_enabled = true
       depends_on_locations      = false
+      policy_state              = "enabledForReportingButNotEnforced"
     }
 
     network_block = {
@@ -24,6 +25,7 @@ locals {
       excluded_groups       = data.azuread_groups.break_glass_groups.object_ids
       built_in_controls     = ["block"]
       depends_on_locations  = true
+      policy_state          = "enabledForReportingButNotEnforced"
     }
 
     legacy_authentication_block = {
@@ -35,6 +37,7 @@ locals {
       excluded_groups       = data.azuread_groups.break_glass_groups.object_ids
       built_in_controls     = ["block"]
       depends_on_locations  = false
+      policy_state          = "enabledForReportingButNotEnforced"
     }
 
     secure_security_info_registration = {
@@ -50,6 +53,7 @@ locals {
       exclude_guests        = true
       built_in_controls     = ["mfa"]
       depends_on_locations  = true
+      policy_state          = "enabledForReportingButNotEnforced"
     }
 
     restrict_device_code_flow = {
@@ -61,11 +65,11 @@ locals {
       excluded_groups       = data.azuread_groups.break_glass_groups.object_ids
       built_in_controls     = ["block"]
       depends_on_locations  = false
+      policy_state          = "enabledForReportingButNotEnforced"
     }
 
     phishing_resistant_mfa = {
       policy_name                       = "CA - Require Phishing-Resistant MFA for Admins"
-      policy_state                      = var.reporting_only_for_all_policies ? "enabledForReportingButNotEnforced" : "enabled"
       client_app_types                  = ["all"]
       included_applications             = ["All"]
       excluded_applications             = []
@@ -73,11 +77,11 @@ locals {
       excluded_groups                   = data.azuread_groups.break_glass_groups.object_ids
       grant_operator                    = "AND"
       authentication_strength_policy_id = "/policies/authenticationStrengthPolicies/<policy GUID>" # Look up Phishing-resistant MFA strength policy GUID: https://learn.microsoft.com/en-us/entra/identity/authentication/concept-authentication-strengths#built-in-authentication-strengths
+      policy_state                      = "enabledForReportingButNotEnforced"
     }
 
     passwordless_mfa = {
       policy_name                       = "CA - Require Passwordless MFA"
-      policy_state                      = var.reporting_only_for_all_policies ? "enabledForReportingButNotEnforced" : "enabled"
       client_app_types                  = ["all"]
       included_applications             = ["All"]
       excluded_applications             = []
@@ -85,6 +89,7 @@ locals {
       excluded_groups                   = data.azuread_groups.break_glass_groups.object_ids
       grant_operator                    = "AND"
       authentication_strength_policy_id = "/policies/authenticationStrengthPolicies/<policy GUID>" # Look up Passwordless MFA strength policy GUID: https://learn.microsoft.com/en-us/entra/identity/authentication/concept-authentication-strengths#built-in-authentication-strengths
+      policy_state                      = "enabledForReportingButNotEnforced"
     }
   }
 }
